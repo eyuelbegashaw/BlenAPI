d8e17063cc0162ff3c95ac57564bfcbec5c113af
Eyuel Begashaw <eyuelbegashaw214@example.com>
2023-06-12 13:33:11 +0300

    try catch

--- app.py
+++ app.py
@@ -16,30 +16,39 @@ def check():
 
 @app.route("/cardRecognition")
 def hello_world():
-    if 'image' not in request.files:
-        return "No image found in request", 400
+    try:
+        if 'image' not in request.files:
+            return "No image found in request", 400
+        
+        image = request.files['image']
+        if image.filename == '':
+            return "Empty image filename", 400
+        print("IMAGE RECEIVED")
 
-    image = request.files['image']
-    if image.filename == '':
-        return "Empty image filename", 400
+        #prediction using model
+        results = model.predict(image)
 
-    #prediction using model
-    results = model.predict(image)
+        #coordinates of the card
+        x = results[0].boxes.data[0]
+        x1 = x[0].item()
+        y1 = x[1].item()
+        x2 = x[2].item()
+        y2 = x[3].item()
 
-    #coordinates of the card
-    x = results[0].boxes.data[0]
-    x1 = x[0].item()
-    y1 = x[1].item()
-    x2 = x[2].item()
-    y2 = x[3].item()
+        print("coordinates received")
+        print(result)
 
-    #crop the detected image
-    cropped_image = image.crop((x1, y1, x2, y2))
+        #crop the detected image
+        cropped_image = image.crop((x1, y1, x2, y2))
 
-    #Perform ocr on the cropped image
-    reader = easyocr.Reader(['en'] , gpu=False)
-    arrayImage = np.asarray(cropped_image)
-    result = reader.readtext(arrayImage, allowlist ='0123456789' ,detail = 0)
-    cleanedNumbers = "".join(result)
+        #Perform ocr on the cropped image
+        reader = easyocr.Reader(['en'] , gpu=False)
+        arrayImage = np.asarray(cropped_image)
+        result = reader.readtext(arrayImage, allowlist ='0123456789' ,detail = 0)
+        cleanedNumbers = "".join(result)
 
-    return jsonify({"cardNumbers" : cleanedNumbers}) 
+        return jsonify({"cardNumbers" : cleanedNumbers}) 
+    except Exception as error:
+        print("An exception occurred:", error) # An exception occurred: division by zero
+
+    
